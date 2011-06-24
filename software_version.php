30f06fecd1ca43d8ba124fbe9d2f24669a0907bc
Stephen Yeargin <stephen.yeargin@gmail.com>
2011-06-23 23:42:42 -0500

    Update link to point to GitHub.

--- software_version.php
+++ software_version.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Software Versions
 Version: 1.0
-Plugin URI: http://stephenyeargin.com/blog/tag/plugins/
+Plugin URI: https://github.com/stephenyeargin/wp-software-version
 Description: Versions of server software returned in post or page content if <code>[version:{software}]</code> or <code>[version&nbsp;type='{software}']</code> tag (2.5.1 or later) is included. Available software: <code>php, mysql, wordpress</code>.
 Author: Stephen Yeargin
 Author URI: http://stephenyeargin.com
